008b72d9c02713661fba3c072209f3ae11e99958
Swayam101 <swayamprajapat21@gmail.com>
2022-11-01 10:36:59 +0530

    Final

--- script.js
+++ script.js
@@ -51,15 +51,15 @@ const countdown = () => {
     
   var event_day = new Date(present_date.getFullYear(), 10, 15)
     
-  if (present_date.getMonth() == 10 && present_date.getdate() > 15)
+  if (present_date.getMonth() == 10 && present_date.getDate() > 15)
       event_day.setFullYear(event_day.getFullYear() + 1)
     
   var Result = Math.round(event_day.getTime() - present_date.getTime()) / (one_day);
     
   var Final_Result = Result.toFixed(0)-1;
 
-    document.querySelector(".days-remaining span").innerText = Final_Result;
-
+    document.querySelector(".mySpan").innerHTML = Final_Result;
+   console.log("Yes This Function is Executing");
 }
 
 countdown()
